099330a4a2d64d4656a3517eac9263f7201499d7
But Thou Must <dqmanualscans@gmail.com>
2025-09-18 20:18:50 -0400

    Use more pc() asserts, loopify credits data insertion

--- asm/insert graphics.asm
+++ asm/insert graphics.asm
@@ -7,32 +7,43 @@ lorom
 ; fit into the space for the original data, so do not need to do any repointing
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; replace title logo's graphics and tilemap
-; tileset must be <= 2116 bytes; tilemap must be <= 154 bytes
-org $07CC6B
-    incbin "graphics/recompressed TL'd title logo TILES.bin"
-org $07D4B5
-    incbin "graphics/recompressed TL'd title logo TILEMAP.bin"
+!TitleLogoTilesetPtr = $07CC6B
+!TitleLogoTilemapPtr = $07D4B5
+!TitleLogoNumTilesPtr = $07CC67
 
-; update the number of tiles that get copied in, if needed
+; replace title logo's graphics and tilemap
+; tileset must be <= 2116 (0x844) bytes; tilemap must be <= 154 (0x9A) bytes
+org !TitleLogoTilesetPtr
+    incbin "graphics/recompressed TL'd title logo TILES.bin"
+    assert pc() <= !TitleLogoTilesetPtr+$844
+org !TitleLogoTilemapPtr
+    incbin "graphics/recompressed TL'd title logo TILEMAP.bin"
+    assert pc() <= !TitleLogoTilemapPtr+$9A
+
+; update # tiles to copy in, if needed
 ; org $07CC67
 ;     db $8b
 
 ; --------------------
 
+!FileSelectTilesetPtr = $07E637
+!FileSelectTilemapPtr = $07E855
+!FileSelectNumTilesPtr = $07E633
+
 ; replace file select prompts like "begin" or "delete"
-; tileset must be <= 536 bytes; tilemap must be <= 389 bytes
-org $07E637
+; tileset must be <= 536 (0x218) bytes; tilemap must be <= 389 (0x185) bytes
+org !FileSelectTilesetPtr
     ; incbin "graphics/recompressed TL'd file select TILES.bin"
     incbin "graphics/recompressed file select prompts - new EN tileset squish 'D' no file.bin"
-
-; update the number of tiles that get copied in
-org $07E633
-    db $2d
-
-org $07E855
+    assert pc() <= !FileSelectTilesetPtr+$218
+org !FileSelectTilemapPtr
     ; incbin "graphics/recompressed TL'd file select TILEMAP.bin"
     incbin "graphics/recompressed file select prompts - new EN tilemap squish 'D' no file.bin"
+    assert pc() <= !FileSelectTilemapPtr+$185
+
+; update # tiles to copy in
+org !FileSelectNumTilesPtr
+    db $2d
 
 ; circularly shift the palette values back one position
 ;   purpose: versus the stock palette, the palette that superfamiconv generated
@@ -43,34 +54,46 @@ org $07E9E0
 
 ; --------------------
 
-; replace the 4 buttons in the bottom right of the name entry screen
-; notice: I needed no tilemap changes, but including instruction if you do
-; tileset must be <= 1346 bytes; tilemap must be <= 832 bytes
-org $07DD87
-    incbin "graphics/recompressed TL'd name entry TILES.bin"
-; org $07E2F3
-; incbin "graphics/PLACEHOLDER name entry tilemap.bin"
+!NameEntryTilesetPtr = $07DD87
+!NameEntryTilemapPtr = $07E2F3
+!NameEntryNumTilesPtr = $07DD83
 
-; update the number of tiles that get copied in, if needed
-; org $07DD83
+; replace the 4 buttons in the bottom right of the name entry screen
+; tileset must be <= 1346 (0x542) bytes; tilemap must be <= 832 (0x340) bytes
+org !NameEntryTilesetPtr
+    incbin "graphics/recompressed TL'd name entry TILES.bin"
+    assert pc() <= !NameEntryTilesetPtr+$542
+; I didn't need to edit the tilemap, but including comments if you do
+; org !NameEntryTilemapPtr
+;     incbin "graphics/PLACEHOLDER name entry tilemap.bin"
+;     assert pc() <= !NameEntryTilemapPtr+$340
+
+; update # tiles to copy in, if needed
+; org !NameEntryNumTilesPtr
 ;     db $42
 
 ; --------------------
 
+!NamiTilesetPtr = $07A4EA
+!NamiTilemapPtr = $07AA34
+!NamiNumTilesPtr = $07A4E6
+
 ; replace the graphic that reads "奈美" with one that reads "Nami"
-; tileset must be <= 1336 bytes; tilemap must be <= 163 bytes
-org $07A4EA
+; tileset must be <= 1336 (0x538) bytes; tilemap must be <= 163 (0xA3) bytes
+org !NamiTilesetPtr
     ; incbin "graphics/recompressed TL'd nami TILES 5bpp.bin"
     incbin "graphics/recompressed TL'd nami TILES 2bpp.bin"
-org $07AA34
+    assert pc() <= !NamiTilesetPtr+$538
+org !NamiTilemapPtr
     incbin "graphics/recompressed TL'd nami TILEMAP.bin"
+    assert pc() <= !NamiTilemapPtr+$A3
 
 ; update the number of tiles that are copied in, if needed
-org $07A4E6
+org !NamiNumTilesPtr
     db $42
 
-; to make reinsertion/recompression easier, make it so the 2bpp graphics are in
-; a 2bpp container instead of a 5bpp container when decompressed
+; to make reinsertion/recompression easier, make it so the "container" you get
+; from decompression for the 2bpp graphics is 2bpp large instead of 5bpp large
 org $07A4E9
     ; db $40
     db $10
@@ -81,6 +104,7 @@ org $07A4E9
 ; update which sprite IDs are used, and update a pointer to one of them
 org $02B7BD
     incbin "graphics/new file select highlight rectangles.bin"
+    assert pc() <= $02B839
 org $02BC8D
     dw $B7F7
 
@@ -123,223 +147,123 @@ org $00BFC0
 ; org $01FAFA
     ; dw $7FFF
 
+!NumCreditsTilemaps = 14
+
+OldPtr1 = $44282
+OldPtr2 = $44307
+OldPtr3 = $4436A
+OldPtr4 = $44491
+OldPtr5 = $4450A
+OldPtr6 = $445CC
+OldPtr7 = $44675
+OldPtr8 = $446FE
+OldPtr9 = $447E3
+OldPtr10 = $44880
+OldPtr11 = $4493F
+OldPtr12 = $44A2E
+OldPtr13 = $44B94
+OldPtr14 = $44C3F
+
+!CreditsFile1 = "graphics/recompressed 01 supervisor author RLE TILEMAP 0x17 x 0x07.bin"
+!CreditsFile2 = "graphics/recompressed 02 planner RLE TILEMAP 0x14 x 0x07.bin"
+!CreditsFile3 = "graphics/recompressed 03 writer RLE TILEMAP 0x16 x 0x13.bin"
+!CreditsFile4 = "graphics/recompressed 04 art director RLE TILEMAP 0x14 x 0x07.bin"
+!CreditsFile5 = "graphics/recompressed 05 graphics RLE TILEMAP 0x15 x 0x0B.bin"
+!CreditsFile6 = "graphics/recompressed 06 composer RLE TILEMAP 0x1A x 0x07.bin"
+!CreditsFile7 = "graphics/recompressed 07 programming director RLE TILEMAP 0x1A x 0x07.bin"
+!CreditsFile8 = "graphics/recompressed 08 programmer RLE TILEMAP 0x16 x 0x0B.bin"
+!CreditsFile9 = "graphics/recompressed 09 sound programmer RLE TILEMAP 0x17 x 0x07.bin"
+!CreditsFile10 = "graphics/recompressed 10 original sfx RLE TILEMAP 0x1B x 0x07.bin"
+!CreditsFile11 = "graphics/recompressed 11 support pg 1 RLE TILEMAP 0x19 x 0x0B.bin"
+!CreditsFile12 = "graphics/recompressed 12 support pg 2 RLE TILEMAP 0x1E x 0x0F.bin"
+!CreditsFile13 = "graphics/recompressed 13 producer director RLE TILEMAP 0x16 x 0x07.bin"
+!CreditsFile14 = "graphics/recompressed 14 (c) chunsoft RLE TILEMAP 0x16 x 0x07.bin"
+
+!CreditsDimens1 = $17,$07
+!CreditsDimens2 = $14,$07
+!CreditsDimens3 = $16,$13
+!CreditsDimens4 = $14,$07
+!CreditsDimens5 = $15,$0B
+!CreditsDimens6 = $1A,$07
+!CreditsDimens7 = $1A,$07
+!CreditsDimens8 = $16,$0B
+!CreditsDimens9 = $17,$07
+!CreditsDimens10 = $1B,$07
+!CreditsDimens11 = $19,$0B
+!CreditsDimens12 = $1E,$0F
+!CreditsDimens13 = $16,$07
+!CreditsDimens14 = $16,$07
+
 !JProm = "rom/Otogirisou (Japan).sfc"
 
-; replace the tileset used for the credits, and update # tiles for it
-; notice no "-> $address" on the incbin; this is to keep the PC's value from
-; directly after the inserted file, so we can assign it to a label
+; replace tileset used for the credits, and update # tiles used
 org $08AA0A
     dw $02D9
     skip $2
     incbin "graphics/recompressed 00 english credits TILES - no empty tile.bin"
     assert pc() <= !PaletteForEndFinPtr
 
-; basic template for setting up new tilemaps: get pointer to start of metadata
-NewCredit1Pointer:
-    ; grab 4 bytes from the original pointer
-    OldPtr1 = $44282
-    incbin "!JProm":(OldPtr1)..(OldPtr1+!NumMetadataBytesToCopy)
-    ; set the new width and height of the tilemap
-    db $17,$07
-    ; insert the new tilemap's binary data
-    incbin "graphics/recompressed 01 supervisor author RLE TILEMAP 0x17 x 0x07.bin"
-    assert pc() <= !PaletteForEndFinPtr
-
-NewCredit2Pointer:
-    OldPtr2 = $44307
-    incbin "!JProm":(OldPtr2)..(OldPtr2+!NumMetadataBytesToCopy)
-    db $14,$07
-    incbin "graphics/recompressed 02 planner RLE TILEMAP 0x14 x 0x07.bin"
-    assert pc() <= !PaletteForEndFinPtr
-
-NewCredit3Pointer:
-    OldPtr3 = $4436A
-    incbin "!JProm":(OldPtr3)..(OldPtr3+!NumMetadataBytesToCopy)
-    db $16,$13
-    incbin "graphics/recompressed 03 writer RLE TILEMAP 0x16 x 0x13.bin"
-    assert pc() <= !PaletteForEndFinPtr
-
-NewCredit4Pointer:
-    OldPtr4 = $44491
-    incbin "!JProm":(OldPtr4)..(OldPtr4+!NumMetadataBytesToCopy)
-    db $14,$07
-    incbin "graphics/recompressed 04 art director RLE TILEMAP 0x14 x 0x07.bin"
-    assert pc() <= !PaletteForEndFinPtr
-
-NewCredit5Pointer:
-    OldPtr5 = $4450A
-    incbin "!JProm":(OldPtr5)..(OldPtr5+!NumMetadataBytesToCopy)
-    db $15,$0B
-    incbin "graphics/recompressed 05 graphics RLE TILEMAP 0x15 x 0x0B.bin"
-    assert pc() <= !PaletteForEndFinPtr
-
-NewCredit6Pointer:
-    OldPtr6 = $445CC
-    incbin "!JProm":(OldPtr6)..(OldPtr6+!NumMetadataBytesToCopy)
-    db $1A,$07
-    incbin "graphics/recompressed 06 composer RLE TILEMAP 0x1A x 0x07.bin"
-    assert pc() <= !PaletteForEndFinPtr
-
-NewCredit7Pointer:
-    OldPtr7 = $44675
-    incbin "!JProm":(OldPtr7)..(OldPtr7+!NumMetadataBytesToCopy)
-    db $1A,$07
-    incbin "graphics/recompressed 07 programming director RLE TILEMAP 0x1A x 0x07.bin"
-    assert pc() <= !PaletteForEndFinPtr
-
-NewCredit8Pointer:
-    OldPtr8 = $446FE
-    incbin "!JProm":(OldPtr8)..(OldPtr8+!NumMetadataBytesToCopy)
-    db $16,$0B
-    incbin "graphics/recompressed 08 programmer RLE TILEMAP 0x16 x 0x0B.bin"
-    assert pc() <= !PaletteForEndFinPtr
-
-NewCredit9Pointer:
-    OldPtr9 = $447E3
-    incbin "!JProm":(OldPtr9)..(OldPtr9+!NumMetadataBytesToCopy)
-    db $17,$07
-    incbin "graphics/recompressed 09 sound programmer RLE TILEMAP 0x17 x 0x07.bin"
-    assert pc() <= !PaletteForEndFinPtr
-
-NewCredit10Pointer:
-    OldPtr10 = $44880
-    incbin "!JProm":(OldPtr10)..(OldPtr10+!NumMetadataBytesToCopy)
-    db $1B,$07
-    incbin "graphics/recompressed 10 original sfx RLE TILEMAP 0x1B x 0x07.bin"
-    assert pc() <= !PaletteForEndFinPtr
-
-NewCredit11Pointer:
-    OldPtr11 = $4493F
-    incbin "!JProm":(OldPtr11)..(OldPtr11+!NumMetadataBytesToCopy)
-    db $19,$0B
-    incbin "graphics/recompressed 11 support pg 1 RLE TILEMAP 0x19 x 0x0B.bin"
-    assert pc() <= !PaletteForEndFinPtr
-
-NewCredit12Pointer:
-    OldPtr12 = $44A2E
-    incbin "!JProm":(OldPtr12)..(OldPtr12+!NumMetadataBytesToCopy)
-    db $1E,$0F
-    incbin "graphics/recompressed 12 support pg 2 RLE TILEMAP 0x1E x 0x0F.bin"
-    assert pc() <= !PaletteForEndFinPtr
-
-NewCredit13Pointer:
-    OldPtr13 = $44B94
-    incbin "!JProm":(OldPtr13)..(OldPtr13+!NumMetadataBytesToCopy)
-    db $16,$07
-    incbin "graphics/recompressed 13 producer director RLE TILEMAP 0x16 x 0x07.bin"
-    assert pc() <= !PaletteForEndFinPtr
-
-NewCredit14Pointer:
-    OldPtr14 = $44C3F
-    incbin "!JProm":(OldPtr14)..(OldPtr14+!NumMetadataBytesToCopy)
-    db $16,$07
-    incbin "graphics/recompressed 14 (c) chunsoft RLE TILEMAP 0x16 x 0x07.bin"
-    assert pc() <= !PaletteForEndFinPtr
+for i = 1..!NumCreditsTilemaps+1
+    ; grab 4 metadata bytes from original ptr, set new tilemap's width/height,
+    ; insert new tilemap's data
+    NewCreditPointer!i:
+        incbin "!JProm":(OldPtr!i)..(OldPtr!i+!NumMetadataBytesToCopy)
+        db !{CreditsDimens!{i}}
+        incbin "!{CreditsFile!{i}}"
+        assert pc() <= !PaletteForEndFinPtr
+endfor
 
 ; If you NEED to repoint the palette data, here is some code to do that for you:
 ; NewCreditsPalettePointer:
-;     incbin "rom/Otogirisou (Japan).sfc":$44D48..$44D4F
+;     incbin "!JProm":$44D48..$44D4F
 ; org $00C19C
 ;     dl NewCreditsPalettePointer
 
 ;;;;;;;;;;;;;;;;;;;;
 
 ; now that all of the tilemaps are inserted with their pointers accounted for,
-; go to each one's structure list and update their pointers
-; also update the Y/X tile positions of each tilemap on screen
+; go to the structure list for each one, and update both their pointers and the
+; Y/X tile positions of each tilemap on screen
 
 ; I suggest keeping the X positions as consistent as possible, and centering the 
 ; Y positions based on a screen height of 224 pixels = 1C tiles.
 ; Tilemap heights I used (7, B, F, 13) -> Y positions (A, 8, 6, 4)
 ; However, keep the 1st credit at Y position 9 so it appears on cue with music.
 
-; A tilemap structure is 10 bytes. The first three bytes are the pointer. The
-; last byte is the Y/X position on screen. So six bytes are ignored.
+; Tilemap structures are each 10 bytes. Bytes 0-2 are the pointer.
+; Byte 9 is the Y/X position on screen. So six bytes are ignored.
 !SkipBytes = $A-1-3
 
-; credit 1
+CreditsXY1 = $93
+CreditsXY2 = $A3
+CreditsXY3 = $43
+CreditsXY4 = $A3
+CreditsXY5 = $83
+CreditsXY6 = $A3
+CreditsXY7 = $A3
+CreditsXY8 = $83
+CreditsXY9 = $A3
+CreditsXY10 = $A3
+CreditsXY11 = $83
+CreditsXY12 = $61
+CreditsXY13 = $A3
+CreditsXY14 = $95
+
+; credit 1 needs to be handled separately because it's clumped together with
+; more structures in its list
 org $00C192
-    dl NewCredit1Pointer
+    dl NewCreditPointer1
     skip !SkipBytes
-    db $93
+    db CreditsXY1
 
-; credit 2
-org $00C1A4
-    dl NewCredit2Pointer
-    skip !SkipBytes
-    db $A3
-
-; credit 3
-org $00C1AF
-    dl NewCredit3Pointer
-    skip !SkipBytes
-    db $43
-
-; credit 4
-org $00C1BA
-    dl NewCredit4Pointer
-    skip !SkipBytes
-    db $A3
-
-; credit 5
-org $00C1C5
-    dl NewCredit5Pointer
-    skip !SkipBytes
-    db $83
-
-; credit 6
-org $00C1D0
-    dl NewCredit6Pointer
-    skip !SkipBytes
-    db $A3
-
-; credit 7
-org $00C1DB
-    dl NewCredit7Pointer
-    skip !SkipBytes
-    db $A3
-
-; credit 8
-org $00C1E6
-    dl NewCredit8Pointer
-    skip !SkipBytes
-    db $83
-
-; credit 9
-org $00C1F1
-    dl NewCredit9Pointer
-    skip !SkipBytes
-    db $A3
-
-; credit 10
-org $00C1FC
-    dl NewCredit10Pointer
-    skip !SkipBytes
-    db $A3
-
-; credit 11
-org $00C207
-    dl NewCredit11Pointer
-    skip !SkipBytes
-    db $83
-
-; credit 12
-org $00C212
-    dl NewCredit12Pointer
-    skip !SkipBytes
-    db $61
-
-; credit 13
-org $00C21D
-    dl NewCredit13Pointer
-    skip !SkipBytes
-    db $A3
-
-; credit 14
-org $00C228
-    dl NewCredit14Pointer
-    skip !SkipBytes
-    db $95
+; loop over the structure lists for credits 2-14 (all are same size, 0xB)
+; each list starts with a byte to indicate 1 structure, which is 0xA bytes large
+for i = 2..!NumCreditsTilemaps+1
+    org $00C1A4+(!i-2)*$B
+        dl NewCreditPointer!i
+        skip !SkipBytes
+        db CreditsXY!i
+endfor
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Translate 終 to "END" and 完 to "FIN"
@@ -358,12 +282,14 @@ org $08CD50
     dw $0037
 org $08CD54
     incbin "graphics/recompressed 15 END TILES - no empty tile.bin"
+    assert pc() <= $08CFDA
 
 ; change width and height, and insert new tilemap
 org $08CFDE
     db $0C,$05
 org $08CFE0
     incbin "graphics/recompressed 15 END tilemap 0x0C x 0x05.bin"
+    assert pc() <= $08D082
 
 ;;;;;;;;;;;;;;;;;;;;
 
@@ -377,8 +303,10 @@ org $08D082
     dw $002E
 org $08D086
     incbin "graphics/recompressed 16 FIN TILES - no empty tile.bin"
+    assert pc() <= $08D2F8
 
 org $08D2FC
     db $0B,$05
 org $08D2FE
     incbin "graphics/recompressed 16 FIN tilemap 0x0B x 0x05.bin"
+    assert pc() <= $08D39B
